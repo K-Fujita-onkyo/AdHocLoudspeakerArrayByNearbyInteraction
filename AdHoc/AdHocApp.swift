4504a27c5764732a0ffb2e6909c4a3d4d760d36f
K-Fujita-onkyo <m5261108.aizu@gmail.com>
2023-10-31 06:44:23 +0900

    Initial Commit

---
+++ AdHoc/AdHocApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdHocApp.swift
+//  AdHoc
+//
+//  Created by 藤田一旗 on 2023/10/31.
+//
+
+import SwiftUI
+
+@main
+struct AdHocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
